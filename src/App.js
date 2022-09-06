8a7a7399711ca3a5de8ef34074b80daf0ef8fa82
Richard Rivera <rivera.richard92@yahoo.com>
2022-09-06 12:44:41 -0400

    Added img

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
 			<div className="outer-rectangle center">
 				<div className="top-rectangle top-center"></div>
 				<div className="inner-rectangle inner-center"></div>
+				<div className="socials-center">
+					<div className="socials">LinkedIn</div>
+					<div className="socials">Github</div>
+					<div className="socials">Mail</div>
+				</div>
 			</div>
 			<div className="circle circle-center"></div>
 			<div className="inner-circle circle-center"></div>
