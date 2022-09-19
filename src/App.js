d4410c3b37386f6d49439779af734195e457f121
Richard Rivera <rivera.richard92@yahoo.com>
2022-09-18 22:46:59 -0400

    Added Name, About Me & Roles Section

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import {
+// 	solid,
+// 	regular,
+// 	brands,
+// 	icon,
+// } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 function App() {
 	return (
 		<div>
 			<div className="outer-rectangle center">
 				<div className="top-rectangle top-center"></div>
-				<div className="inner-rectangle inner-center"></div>
+				<div className="circle circle-center"></div>
+				<div className="inner-circle circle-center"></div>
 				<div className="socials-center">
-					<div className="socials">LinkedIn</div>
-					<div className="socials">Github</div>
-					<div className="socials">Mail</div>
+					<div className="social">
+						<FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+					</div>
+					<div className="social">
+						<FontAwesomeIcon icon={faGithub} size="lg" />
+					</div>
+					<div className="social">
+						<FontAwesomeIcon icon={faEnvelope} size="lg" />
+					</div>
+				</div>
+				<div className="inner-rectangle inner-center">
+					<p className="name">Richard Rivera</p>
+					<hr />
+					<p className="about-title">About Me</p>
+					<p className="about">Random Text</p>
+					<p className="roles-title">Prev. Roles</p>
+					<div className="roles-align">
+						<p className="role">🔵 Game Developer</p>
+						<p className="role">🔵 WordPress Developer</p>
+						<p className="role">🔵 CST Tutor</p>
+					</div>
 				</div>
 			</div>
-			<div className="circle circle-center"></div>
-			<div className="inner-circle circle-center"></div>
 		</div>
 	);
 }
